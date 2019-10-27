67611e69cdb4def033ac3782323985b3ac1cb027
Vet86 <vitalyev_aleksey@mail.ru>
2019-10-27 11:33:24 +0300

    Hello SignalR

---
+++ FirstApp/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace FirstApp
+{
+    public class ChatHub : Hub
+    {
+        public void Hello(string message)
+        {
+            Clients.All.Hello(message);
+        }
+    }
+}

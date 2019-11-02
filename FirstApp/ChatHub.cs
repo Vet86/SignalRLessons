41fd5c74733df1e38d5397a3ad39703116250bc2
Vet86 <vitalyev_aleksey@mail.ru>
2019-11-02 16:49:57 +0300

    ~ First commit

--- FirstApp/ChatHub.cs
+++ FirstApp/ChatHub.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace FirstApp
 {
+    [HubName("chatHub")]
     public class ChatHub : Hub
     {
         public void Hello(string message)
--- FirstApp/Scripts/SignalR.js
+++ FirstApp/Scripts/SignalR.js
@@ -1,3 +1,11 @@
-﻿$.connection.hub.start()
-    .done(function () { console.log("It worked!") })
+﻿var chat = $.connection.chatHub;
+$.connection.hub.start()
+    .done(function () {
+        console.log("It worked!");
+        chat.server.hello("asdasd");
+    })
     .fail(function () { alert("ERROR") });
+
+$.chat.client.addNewMessageToPage = function (message) {
+    alert(message);
+};
